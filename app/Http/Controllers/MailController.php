ecb4adba5058c2630d04c5d54324bf334c82672e
Adailton Moura <adailton.silva091@academico.ifs.edu.br>
2020-09-24 21:25:03 -0300

    Send multiple files

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -8,13 +8,28 @@ use Illuminate\Support\Facades\Mail as FacadesMail;
 
 class MailController extends Controller
 {
-    public function teste(Request $request){
+    public function sendMail(Request $request){
+        
+        $paths = array();
+        
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'content' => 'required',
+            'file' => 'required',
+        ]);
 
-        $path = $request->file('file')->store('files');
+        if($validatedData){
+            for ($i = 0; $i < count($request->allFiles()['files']); $i++) { 
+                $file = $request->allFiles()['files'][$i];
+    
+                $paths[$i] = $file->store('files');
+            }
+            FacadesMail::send(new Mail($paths));
+            return redirect()->route('home')->with('status', 'Email Enviado');
+        }
+            return redirect()->route('home')->with('status', 'Selecione o(s) arquivo (s)');
+        
 
-
-        FacadesMail::send(new Mail($path));
-        return redirect()->route('home')->with('status', 'Email Enviado');
 
     }
 }
